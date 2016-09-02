e222369110b730aedb691c5d587513fc37f1cc5e
Jonathan Ellington <ellingtonjp@gmail.com>
2016-09-01 17:11:05 -0700

    Rename + support for 60 bit frames

--- tests/test_IrigTTLEncoder.py
+++ tests/test_IrigTTLEncoder.py
@@ -4,13 +4,13 @@ import random
 
 PERIOD = 1000
 
-def bench():
+def bench(num_bits):
   rst = ResetSignal(0, active=1, async=True)
   frame_latched, ttl_out, enable, clk = [Signal(bool(0)) for i in range(4)]
-  next_frame = Signal(intbv(0)[100:])
+  next_frame = Signal(intbv(0)[num_bits:])
 
-  dut = hardware.IrigTTLOutput(next_frame, frame_latched, ttl_out, enable, clk, rst)
-  frame = Signal(intbv(0)[100:])
+  dut = hardware.IrigTTLEncoder(next_frame, frame_latched, ttl_out, enable, clk, rst, num_bits=num_bits)
+  frame = Signal(intbv(0)[num_bits:])
 
   @always(delay(PERIOD//2))
   def clkgen():
@@ -26,29 +26,27 @@ def bench():
     while True:
       yield ttl_out.posedge
       pulse_count = 0 
-      
+
       yield clk.negedge
       while ttl_out:
         pulse_count += 1
         yield clk.negedge
 
-      print(pulse_count)
+  @instance
+  def monitor():
+    while True:
+      yield clk.negedge
 
-  # @instance
-  # def monitor():
-  #   while True:
-  #     yield clk.negedge
-  #
-  #     if enable and not rst:
-  #       if frame_latched:
-  #         index = 0
-  #       elif request:
-  #         index += 1
-  #
-  #       if index == 0 or index % 10 == 9:
-  #         assert irig_bit == 2
-  #       else:
-  #         assert irig_bit == frame[index]
+      if enable and not rst:
+        if frame_latched:
+          index = 0
+        elif request:
+          index += 1
+
+        if index == 0 or index % 10 == 9:
+          assert irig_bit == 2
+        else:
+          assert irig_bit == frame[index]
 
   @instance
   def stimulus():
@@ -57,7 +55,7 @@ def bench():
     yield clk.negedge 
     rst.next = bool(0)
 
-    frame = utilities.random_frame().replace('_','0')
+    frame = utilities.random_frame(num_bits).replace('_','0')
     next_frame.next = int(frame, 2)
 
     yield clk.negedge 
@@ -66,17 +64,29 @@ def bench():
     num_frames = 10
     for i in range(num_frames): 
       yield frame_latched.negedge  # wait til frame is latched before changing it
-      frame = utilities.random_frame().replace('_','0')
+      frame = utilities.random_frame(num_bits).replace('_','0')
       next_frame.next = int(frame, 2)
 
     raise StopSimulation
 
   return dut, clkgen, get_frame, pulse_counter, stimulus
 
-def test_bench():
-  sim = traceSignals(bench)
-  sim = Simulation(sim)
+def test_bench_100bits():
+  # sim = traceSignals(bench, num_bits=100)
+  # sim = Simulation(sim)
+  sim = Simulation(bench, num_bits=100)
+  sim.run()
+
+def test_bench_60bits():
+  # sim = traceSignals(bench, num_bits=60)
+  # sim = Simulation(sim)
+  sim = Simulation(bench, num_bits=60)
   sim.run()
 
 if __name__ == '__main__':
-  test_bench()
+  # Visual test for IRIG-H
+  PERIOD=100000000
+  sim = traceSignals(bench, num_bits=60)
+  sim = Simulation(sim)
+  sim.run()
+
